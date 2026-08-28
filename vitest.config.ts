33bbeaaec23b9d90cf1afe4a84d9e69ad8b99083
Stefan Liu <aspivis.mentor@gmail.com>
2026-08-28 14:52:59 -0700

    fix: ignore isolated worktrees in checks

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,5 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   resolve: { alias: { '@': new URL('./src', import.meta.url).pathname } },
-  test: { environment: 'node', exclude: [...configDefaults.exclude, 'tests/e2e/**'] },
+  test: { environment: 'node', exclude: [...configDefaults.exclude, '.worktrees/**', 'tests/e2e/**'] },
 })
